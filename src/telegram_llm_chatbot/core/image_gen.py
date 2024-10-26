62e38955a3391d623891fae6ea0f65f1831a3c50
worker-one <konst.verner.org@gmail.com>
2024-10-26 13:44:52 +0200

    wip

--- src/telegram_llm_chatbot/core/image_gen.py
+++ src/telegram_llm_chatbot/core/image_gen.py
@@ -1,16 +1,16 @@
-from llm_chatbot_api.api.schemas import ImageModelConfig
+from telegram_llm_chatbot.api.schemas import DalleConfig
 from openai import OpenAI
 
 
 class Dalle3OpenAI:
     """Wrapper class for the OpenAI DALL-E model."""
 
-    def __init__(self, config: ImageModelConfig):
+    def __init__(self, config: DalleConfig):
         """Initialize the DALL-E model."""
         self.config = config
         self.client = OpenAI()
 
-    def update_config(self, config: ImageModelConfig) -> None:
+    def update_config(self, config: DalleConfig) -> None:
         """Update the configuration of the model."""
         for attr in ["model_name", "n", "quality", "size"]:
             if getattr(config, attr) is not None:
